0e5cea7be96a270a306d5505a53fc432a607646a
Sean <seanwagoner@icloud.com>
2025-09-03 23:13:50 -0400

    Added Marco

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import marco from './assets/marco.jpg'
 
 function App() {
 
   return (
       <div style={{ textAlign: "center", marginTop: "50px" }}>
-        <h1>DOTA Band</h1>
+        {/* <h1>DOTA Band</h1> */}
+        <img src={marco} alt="Marco" width="700" />
       </div>
   )
 }
